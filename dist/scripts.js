868d899f849c66c9a24ea46fe6bc286a897176b6
Wil Arthur <will.arthur@hotmail.com>
2025-12-15 14:00:57 +0000

    Components page and accordions

--- dist/scripts.js
+++ dist/scripts.js
@@ -6,3 +6,20 @@ function toggleMenu() {
   mobileMenu.classList.toggle("translate-x-full");
   htmlTag.classList.toggle("overflow-hidden");
 }
+
+/* Accordions */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
